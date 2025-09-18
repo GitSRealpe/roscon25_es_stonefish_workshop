ffe14a39b3e5831acb2f0bc26c8f3671bf5d3d80
GitSRealpe <msn-realpe@hotmail.com>
2025-09-18 18:36:37 +0200

    writing to vel controller

--- roscontrol_test/scripts/publish_chain.py
+++ roscontrol_test/scripts/publish_chain.py
@@ -4,6 +4,7 @@ import rclpy
 from rclpy.node import Node, Publisher, Timer
 
 from control_msgs.msg import MultiDOFCommand
+from geometry_msgs.msg import Twist
 
 
 class ChainPub(Node):
@@ -12,17 +13,13 @@ class ChainPub(Node):
         super().__init__("chain_node_pub")
 
         self.publisher: Publisher = self.create_publisher(
-            MultiDOFCommand, "pid_controller/reference", 10
+            Twist, "/auv_velocity_controller/body_velocity_command", 10
         )
 
-        self.msg = MultiDOFCommand()
-        self.msg.dof_names = [
-            "auv_velocity_controller/x",
-            "auv_velocity_controller/yaw",
-        ]
-        self.msg.values = [1.0, 5.0]
+        self.msg = Twist()
+        self.msg.linear.x = 1.0
 
-        self.timer: Timer = self.create_timer(0.5, self.timer_callback)
+        self.timer: Timer = self.create_timer(0.1, self.timer_callback)
 
     def timer_callback(self):
         self.publisher.publish(self.msg)
