d58cf067ee0ecdb7e68c3f0166a20bd942bd1db7
GitSRealpe <msn-realpe@hotmail.com>
2025-10-03 18:27:41 +0200

    body pose controller

--- auv_controllers/include/auv_controllers/BodyPoseController.hpp
+++ auv_controllers/include/auv_controllers/BodyPoseController.hpp
@@ -5,24 +5,32 @@
 #include "rclcpp/rclcpp.hpp"
 #include "controller_interface/chainable_controller_interface.hpp"
 #include "realtime_tools/realtime_thread_safe_box.hpp"
-#include "geometry_msgs/msg/twist_stamped.hpp"
-#include <eigen3/Eigen/Dense>
-#include <auv_controllers/auv_velocity_controller_params.hpp>
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <Eigen/Core>
+#include <auv_controllers/auv_pose_controller_params.hpp>
 namespace auv_controllers
 {
     class BodyPoseController : public controller_interface::ChainableControllerInterface
     {
     private:
-        geometry_msgs::msg::TwistStamped twist_command;
-        geometry_msgs::msg::TwistStamped twist_state;
-        realtime_tools::RealtimeThreadSafeBox<geometry_msgs::msg::TwistStamped> rt_command_;
-        realtime_tools::RealtimeThreadSafeBox<geometry_msgs::msg::TwistStamped> rt_state_;
-        rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr state_sub;
-        rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr twist_sub;
+        geometry_msgs::msg::PoseStamped pose_command;
+        geometry_msgs::msg::PoseStamped pose_state;
+        realtime_tools::RealtimeThreadSafeBox<geometry_msgs::msg::PoseStamped> rt_command_;
+        realtime_tools::RealtimeThreadSafeBox<geometry_msgs::msg::PoseStamped> rt_state_;
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr state_sub;
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub;
 
         // Parameters from ROS for
-        std::shared_ptr<auv_velocity_controller::ParamListener> param_listener_;
-        auv_velocity_controller::Params params_;
+        std::shared_ptr<auv_pose_controller::ParamListener> param_listener_;
+        auv_pose_controller::Params params_;
+
+        tf2::Transform mat_current;
+        tf2::Transform mat_goal;
+        tf2::Transform error;
+        double err_roll, err_pitch, err_yaw, integral_x, integral_y, integral_z, last_error_x, last_error_y, last_error_z, dt = 0;
+        rclcpp::Time prev_t;
+        Eigen::Matrix3d pid_err;
 
     public:
         BodyPoseController(/* args */);
--- auv_controllers/src/BodyPoseController.cpp
+++ auv_controllers/src/BodyPoseController.cpp
@@ -1,12 +1,13 @@
 #include <auv_controllers/BodyPoseController.hpp>
 #include "hardware_interface/system_interface.hpp"
+// #include <tf2_eigen/tf2_eigen.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 using std::placeholders::_1;
 namespace auv_controllers
 {
     BodyPoseController::BodyPoseController(/* args */)
     {
-        std::cout << "constructing controller\n";
     }
 
     BodyPoseController::~BodyPoseController()
@@ -18,7 +19,7 @@ namespace auv_controllers
         try
         {
             // Create the parameter listener and get the parameters
-            param_listener_ = std::make_shared<auv_velocity_controller::ParamListener>(get_node());
+            param_listener_ = std::make_shared<auv_pose_controller::ParamListener>(get_node());
             params_ = param_listener_->get_params();
         }
         catch (const std::exception &e)
@@ -32,30 +33,34 @@ namespace auv_controllers
 
     controller_interface::CallbackReturn BodyPoseController::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
     {
-        RCLCPP_INFO(get_node()->get_logger(), "Vel state topic on: %s", params_.body_velocity_state_topic.c_str());
+        RCLCPP_INFO(get_node()->get_logger(), "Current Pose state topic on: %s", params_.body_pose_state_topic.c_str());
         // specify namespace in node launch
-        state_sub = get_node()->create_subscription<geometry_msgs::msg::TwistStamped>(
-            params_.body_velocity_state_topic, rclcpp::SystemDefaultsQoS(),
-            [this](const geometry_msgs::msg::TwistStamped::SharedPtr msg)
+        state_sub = get_node()->create_subscription<geometry_msgs::msg::PoseStamped>(
+            params_.body_pose_state_topic, rclcpp::SystemDefaultsQoS(),
+            [this](const geometry_msgs::msg::PoseStamped::SharedPtr msg)
             {
                 const auto cmd = *msg;
                 rt_state_.set(cmd);
             });
 
-        RCLCPP_INFO(get_node()->get_logger(), "Vel command topic on: ~/body_velocity_command");
+        RCLCPP_INFO(get_node()->get_logger(), "Pose command topic on: ~/body_pose_command");
         // specify namespace in node launch
-        twist_sub = get_node()->create_subscription<geometry_msgs::msg::TwistStamped>(
-            "~/body_velocity_command", rclcpp::SystemDefaultsQoS(),
-            [this](const geometry_msgs::msg::TwistStamped::SharedPtr msg)
+        pose_sub = get_node()->create_subscription<geometry_msgs::msg::PoseStamped>(
+            "~/body_pose_command", rclcpp::SystemDefaultsQoS(),
+            [this](const geometry_msgs::msg::PoseStamped::SharedPtr msg)
             {
                 const auto cmd = *msg;
                 rt_command_.set(cmd);
             });
 
         // Allocate reference interfaces, 6 DOF, 0.0 initialization
-        reference_interfaces_.resize(6, 0.0);
+        reference_interfaces_.resize(7, 0.0);
         // Allocate state interfaces, 6 DOF, 0.0 initialization
-        state_interfaces_values_.resize(6, 0.0);
+        state_interfaces_values_.resize(7, 0.0);
+
+        // initialize pid vars
+        pid_err.setZero();
+        prev_t = rclcpp::Time(0);
 
         return controller_interface::CallbackReturn::SUCCESS;
     }
@@ -82,8 +87,8 @@ namespace auv_controllers
 
         std::vector<std::string> conf_names;
 
-        conf_names.push_back(get_node()->get_name() + std::string("/x/") + hardware_interface::HW_IF_VELOCITY);
-        conf_names.push_back(get_node()->get_name() + std::string("/yaw/") + hardware_interface::HW_IF_VELOCITY);
+        conf_names.push_back(get_name() + std::string("/x/") + hardware_interface::HW_IF_VELOCITY);
+        conf_names.push_back(get_name() + std::string("/yaw/") + hardware_interface::HW_IF_VELOCITY);
 
         return {controller_interface::interface_configuration_type::INDIVIDUAL, conf_names};
 
@@ -105,7 +110,7 @@ namespace auv_controllers
     bool BodyPoseController::on_set_chained_mode(bool /*chained_mode*/)
     {
         RCLCPP_INFO(get_node()->get_logger(), "Controller is now in chained mode");
-        twist_sub.reset();
+        pose_sub.reset();
         return true;
     }
 
@@ -117,33 +122,31 @@ namespace auv_controllers
 
         state_interfaces.emplace_back(
             hardware_interface::StateInterface(
-                export_prefix, std::string("x/") + hardware_interface::HW_IF_VELOCITY,
-                &state_interfaces_values_[0]));
+                export_prefix, std::string("position/x"), &state_interfaces_values_[0]));
 
         state_interfaces.emplace_back(
             hardware_interface::StateInterface(
-                export_prefix, std::string("y/") + hardware_interface::HW_IF_VELOCITY,
-                &state_interfaces_values_[1]));
+                export_prefix, std::string("position/y"), &state_interfaces_values_[1]));
 
         state_interfaces.emplace_back(
             hardware_interface::StateInterface(
-                export_prefix, std::string("z/") + hardware_interface::HW_IF_VELOCITY,
-                &state_interfaces_values_[2]));
+                export_prefix, std::string("position/z"), &state_interfaces_values_[2]));
 
         state_interfaces.emplace_back(
             hardware_interface::StateInterface(
-                export_prefix, std::string("roll/") + hardware_interface::HW_IF_VELOCITY,
-                &state_interfaces_values_[3]));
+                export_prefix, std::string("orientation/x"), &state_interfaces_values_[3]));
 
         state_interfaces.emplace_back(
             hardware_interface::StateInterface(
-                export_prefix, std::string("pitch/") + hardware_interface::HW_IF_VELOCITY,
-                &state_interfaces_values_[4]));
+                export_prefix, std::string("orientation/y"), &state_interfaces_values_[4]));
 
         state_interfaces.emplace_back(
             hardware_interface::StateInterface(
-                export_prefix, std::string("yaw/") + hardware_interface::HW_IF_VELOCITY,
-                &state_interfaces_values_[5]));
+                export_prefix, std::string("orientation/z"), &state_interfaces_values_[5]));
+
+        state_interfaces.emplace_back(
+            hardware_interface::StateInterface(
+                export_prefix, std::string("orientation/w"), &state_interfaces_values_[6]));
 
         return state_interfaces;
     }
@@ -156,78 +159,126 @@ namespace auv_controllers
 
         reference_interfaces.push_back(
             hardware_interface::CommandInterface(
-                export_prefix, std::string("x/") + hardware_interface::HW_IF_VELOCITY,
-                &reference_interfaces_[0]));
+                export_prefix, std::string("position/x"), &reference_interfaces_[0]));
 
         reference_interfaces.push_back(
             hardware_interface::CommandInterface(
-                export_prefix, std::string("y/") + hardware_interface::HW_IF_VELOCITY,
-                &reference_interfaces_[1]));
+                export_prefix, std::string("position/y"), &reference_interfaces_[1]));
 
         reference_interfaces.push_back(
             hardware_interface::CommandInterface(
-                export_prefix, std::string("z/") + hardware_interface::HW_IF_VELOCITY,
-                &reference_interfaces_[2]));
+                export_prefix, std::string("position/z"), &reference_interfaces_[2]));
 
         reference_interfaces.push_back(
             hardware_interface::CommandInterface(
-                export_prefix, std::string("roll/") + hardware_interface::HW_IF_VELOCITY,
-                &reference_interfaces_[3]));
+                export_prefix, std::string("orientation/x"), &reference_interfaces_[3]));
 
         reference_interfaces.push_back(
             hardware_interface::CommandInterface(
-                export_prefix, std::string("pitch/") + hardware_interface::HW_IF_VELOCITY,
-                &reference_interfaces_[4]));
+                export_prefix, std::string("orientation/y"), &reference_interfaces_[4]));
 
         reference_interfaces.push_back(
             hardware_interface::CommandInterface(
-                export_prefix, std::string("yaw/") + hardware_interface::HW_IF_VELOCITY,
-                &reference_interfaces_[5]));
+                export_prefix, std::string("orientation/z"), &reference_interfaces_[5]));
+
+        reference_interfaces.push_back(
+            hardware_interface::CommandInterface(
+                export_prefix, std::string("orientation/w"), &reference_interfaces_[6]));
 
         return reference_interfaces;
     }
 
     controller_interface::return_type BodyPoseController::update_reference_from_subscribers(
-        const rclcpp::Time &time, const rclcpp::Duration & /*period*/)
+        const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
     {
-        auto twist_command_op = rt_command_.try_get();
-        if (twist_command_op.has_value())
-            twist_command = twist_command_op.value();
+        auto pose_command_op = rt_command_.try_get();
+        if (pose_command_op.has_value())
+            pose_command = pose_command_op.value();
 
-        // watchdog looking thing
-        if ((time - twist_command.header.stamp).seconds() > 2.0)
-            twist_command = geometry_msgs::msg::TwistStamped();
-        // RCLCPP_INFO(get_node()->get_logger(), "activate successful");
-        reference_interfaces_[0] = twist_command.twist.linear.x;
-        reference_interfaces_[1] = twist_command.twist.linear.y;
-        reference_interfaces_[2] = twist_command.twist.linear.z;
-        reference_interfaces_[3] = twist_command.twist.angular.x;
-        reference_interfaces_[4] = twist_command.twist.angular.y;
-        reference_interfaces_[5] = twist_command.twist.angular.z;
+        reference_interfaces_[0] = pose_command.pose.position.x;
+        reference_interfaces_[1] = pose_command.pose.position.y;
+        reference_interfaces_[2] = pose_command.pose.position.z;
+        reference_interfaces_[3] = pose_command.pose.orientation.x;
+        reference_interfaces_[4] = pose_command.pose.orientation.y;
+        reference_interfaces_[5] = pose_command.pose.orientation.z;
+        reference_interfaces_[6] = pose_command.pose.orientation.w;
 
         return controller_interface::return_type::OK;
     }
 
     controller_interface::return_type BodyPoseController::update_and_write_commands(
-        const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
+        const rclcpp::Time &time, const rclcpp::Duration & /*period*/)
     {
-        auto twist_state_op = rt_state_.try_get();
-        if (twist_state_op.has_value())
-            twist_state = twist_state_op.value();
+        auto pose_state_op = rt_state_.try_get();
+        if (pose_state_op.has_value())
+            pose_state = pose_state_op.value();
         // Write exported states
-        state_interfaces_values_[0] = twist_state.twist.linear.x;
-        state_interfaces_values_[1] = twist_state.twist.linear.y;
-        state_interfaces_values_[2] = twist_state.twist.linear.z;
-        state_interfaces_values_[3] = twist_state.twist.angular.x;
-        state_interfaces_values_[4] = twist_state.twist.angular.y;
-        state_interfaces_values_[5] = twist_state.twist.angular.z;
+        state_interfaces_values_[0] = pose_state.pose.position.x;
+        state_interfaces_values_[1] = pose_state.pose.position.y;
+        state_interfaces_values_[2] = pose_state.pose.position.z;
+        state_interfaces_values_[3] = pose_state.pose.orientation.x;
+        state_interfaces_values_[4] = pose_state.pose.orientation.y;
+        state_interfaces_values_[5] = pose_state.pose.orientation.z;
+        state_interfaces_values_[6] = pose_state.pose.orientation.w;
 
-        // Write commands to the hardware interface
-        for (size_t i = 0; i < command_interfaces_.size(); ++i)
+        // watchdog looking thing
+        if ((time.seconds() - pose_command.header.stamp.sec) < 2.0)
         {
-            // casting to void to avoid compiler warning
-            static_cast<void>(command_interfaces_[i].set_value(reference_interfaces_[i]));
+            tf2::fromMsg(pose_state.pose, mat_current);
+            tf2::fromMsg(pose_command.pose, mat_goal);
+            // see the goal w.r.t current frame at every iteration
+            error = mat_current.inverseTimes(mat_goal);
+            // get orientation as rpy
+            tf2::Matrix3x3 m(error.getRotation());
+            m.getRPY(err_roll, err_pitch, err_yaw, 1);
+
+            dt = (time.seconds() - prev_t.seconds());
+
+            for (size_t i = 0; i < 3; i++)
+            {
+                // proportional
+                pid_err(i, 0) = params_.gains.dof_names_map[params_.dof_names[i]].p * error.getOrigin()[i];
+                // derivative smooths
+                pid_err(i, 2) = params_.gains.dof_names_map[params_.dof_names[i]].d * (error.getOrigin()[i] - last_error_x) / dt;
+            }
+            // pid_err(0, 0) = params_.gains.dof_names_map.[params_.dof_names[0]].d * error.getOrigin()[0];
+            // pid_err(1, 0) = params_.gains.dof_names_map[params_.dof_names[1]].d * error.getOrigin().y();
+            // pid_err(2, 0) = params_.gains.dof_names_map.at(0).d * error.getOrigin().z();
+
+            // // integral for steady state error,
+            // integral_x += error.getOrigin().x() * dt;
+            // integral_y += error.getOrigin().y() * dt;
+            // integral_z += error.getOrigin().z() * dt;
+            // pid_err(0, 1) = 0.1 * integral_x;
+            // pid_err(1, 1) = 0.1 * integral_y;
+            // pid_err(2, 1) = 0.1 * integral_z;
+            // // derivative smooths
+            // pid_err(0, 2) = 0.3 * (error.getOrigin().x() - last_error_x) / dt;
+            // pid_err(1, 2) = 0.5 * (error.getOrigin().y() - last_error_y) / dt;
+            // pid_err(2, 2) = 0.5 * (error.getOrigin().z() - last_error_z) / dt;
+            // last_error_x = error.getOrigin().x();
+            // last_error_y = error.getOrigin().y();
+            // last_error_z = error.getOrigin().z();
+
+            // std::cout << "x action:" << std::clamp(pid_err.row(0).sum(), -max_vel, max_vel) << "\n";
+            // std::cout << "y action:" << std::clamp(pid_err.row(1).sum(), -max_vel, max_vel) << "\n";
+            // std::cout << "z action:" << std::clamp(pid_err.row(2).sum(), -max_vel, max_vel) << "\n";
+            // std::cout << "yaw action:" << std::clamp(0.7 * err_yaw, -max_rot_vel, max_rot_vel) << "\n";
+
+            // Write commands to the hardware interface
+            for (size_t i = 0; i < command_interfaces_.size(); ++i)
+            {
+                // casting to void to avoid compiler warning
+                static_cast<void>(command_interfaces_[i].set_value(pid_err.row(i).sum()));
+            }
         }
+        else
+        {
+            for (size_t i = 0; i < command_interfaces_.size(); ++i)
+                static_cast<void>(command_interfaces_[i].set_value(0.0));
+        }
+
+        prev_t = time;
 
         return controller_interface::return_type::OK;
     }
