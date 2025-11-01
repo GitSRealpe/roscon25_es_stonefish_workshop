d549355607eeb2448a46009467989d2100ca1f96
GitSRealpe <msn-realpe@hotmail.com>
2025-11-01 19:46:55 +0100

    init to define image transport

--- auv_architecture/scripts/event_render_node.cpp
+++ auv_architecture/scripts/event_render_node.cpp
@@ -3,7 +3,9 @@
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<EventsRenderer>());
+    auto node = std::make_shared<EventsRenderer>();
+    node->init();
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
