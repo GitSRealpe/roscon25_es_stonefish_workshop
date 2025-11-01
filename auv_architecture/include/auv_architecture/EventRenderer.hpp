9f3b363d79594369ef08d0c1375ca7e7d4346bc0
GitSRealpe <msn-realpe@hotmail.com>
2025-11-01 18:05:08 +0100

    using image transport now

--- auv_architecture/include/auv_architecture/EventRenderer.hpp
+++ auv_architecture/include/auv_architecture/EventRenderer.hpp
@@ -6,18 +6,22 @@
 #include <stonefish_ros2/msg/event_array.hpp>
 #include <cv_bridge/cv_bridge.hpp>
 #include <opencv2/core.hpp>
+#include <image_transport/image_transport.hpp>
 
 class EventsRenderer : public rclcpp::Node
 {
 public:
-    explicit EventsRenderer();
+    EventsRenderer();
+    ~EventsRenderer();
+    void init();
 
 private:
     void dataCallback(const stonefish_ros2::msg::EventArray::SharedPtr message);
     void publishImage(const cv_bridge::CvImage &image);
 
     rclcpp::Subscription<stonefish_ros2::msg::EventArray>::SharedPtr event_sub_;
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_;
+    image_transport::Publisher image_pub_;
+    std::shared_ptr<image_transport::ImageTransport> it_;
 };
 
 #endif // EVENT_RENDERER_HPP_
--- auv_architecture/src/EventRenderer.cpp
+++ auv_architecture/src/EventRenderer.cpp
@@ -1,37 +1,47 @@
-// Base import
-#include <auv_architecture/EventRenderer.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <cv_bridge/cv_bridge.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <opencv2/core.hpp>
-#include <cv_bridge/cv_bridge.hpp>
+#include <auv_architecture/EventRenderer.hpp>
 
 EventsRenderer::EventsRenderer() : rclcpp::Node("events_renderer")
 {
-    // Initialize subscriber and publisher
     event_sub_ = create_subscription<stonefish_ros2::msg::EventArray>(
-        "/events_topic", 10, std::bind(&EventsRenderer::dataCallback, this, std::placeholders::_1));
-    image_pub_ = create_publisher<sensor_msgs::msg::Image>("/image_topic", 10);
+        "/events_topic", 10,
+        std::bind(&EventsRenderer::dataCallback, this, std::placeholders::_1));
+}
+
+EventsRenderer::~EventsRenderer()
+{
+    event_sub_.reset();
+    image_pub_.shutdown();
+}
+
+void EventsRenderer::init()
+{
+    // image_transport::ImageTransport it(shared_from_this());
+    it_ = std::make_shared<image_transport::ImageTransport>(shared_from_this());
+    image_pub_ = it_->advertise("/image_topic", 1);
 }
 
 void EventsRenderer::publishImage(const cv_bridge::CvImage &image)
 {
     sensor_msgs::msg::Image::SharedPtr events_image = image.toImageMsg();
-    image_pub_->publish(*events_image);
+    image_pub_.publish(events_image);
 }
 
 void EventsRenderer::dataCallback(const stonefish_ros2::msg::EventArray::SharedPtr message)
 {
     int num_events = message->events.size();
-    // Save image timestamp
+
     cv_bridge::CvImage render_image;
     if (num_events > 0)
-    {
         render_image.header.stamp = message->events[num_events / 2].ts;
-    }
-    // Create events image
+
     render_image.encoding = "bgr8";
-    render_image.image = cv::Mat(message->height, message->width, CV_8UC3);
-    render_image.image = cv::Scalar(255, 255, 255);
-    // Save events image
+    render_image.image = cv::Mat(message->height, message->width, CV_8UC3, cv::Scalar(255, 255, 255));
+
     for (int i = 0; i < num_events; ++i)
     {
         const int x = message->events[i].x;
@@ -39,7 +49,6 @@ void EventsRenderer::dataCallback(const stonefish_ros2::msg::EventArray::SharedP
         render_image.image.at<cv::Vec3b>(cv::Point(x, y)) =
             (message->events[i].polarity ? cv::Vec3b(255, 0, 0) : cv::Vec3b(0, 0, 255));
     }
-    // TODO: Fix sensor in Stonefish
-    // cv::flip(render_image.image, render_image.image, 0);
+
     publishImage(render_image);
 }
